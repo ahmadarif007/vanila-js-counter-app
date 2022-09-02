c9001360ab6a492dd661ba3cba3c637ecdcf0eab
Ahmad Arif <ahmadarif.0007@gmail.com>
2022-09-02 22:25:24 +0600

    Arif | v-1 | function place on different section

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-var value = 0;
+let value = 0;
 
 const counterEl = document.getElementById("counter");
-// const incrementEl = document.getElementById("increment");
-// const decrementEl = document.getElementById("decrement");
 
 function incrementEl() {
   value = value + 1;
-  //   value++;
   counterEl.innerHTML = value;
 }
 
-// // function decrement() {
-// //   return (value = value - 1);
-// // }
-
-// // document.addEventListener("click", incrementEl);
-// // document.addEventListener("click", decrementEl);
+function decrementEl() {
+  value = value - 1;
+  counterEl.innerHTML = value;
+}
 
 document.getElementById("increment").addEventListener("click", incrementEl);
-
-// document.getElementById("increment").addEventListener("click", () => {
-//   value++;
-//   counterEl.innerHTML = value;
-// });
-
-document.getElementById("decrement").addEventListener("click", () => {
-  value--;
-  //   counterEl.innerHTML = value;
-  counterEl.innerText = value;
-});
-
-document.getElementById("counter").innerHTML = value;
+document.getElementById("decrement").addEventListener("click", decrementEl);
